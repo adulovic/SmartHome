4bb828cfba3338dd9251c3140653e0dbed40a9a7
adulovic <adulovic@platergroup.com>
2018-03-16 10:42:12 -0400

    office node

--- WorkingNodes/node00_Office/GW.h
+++ WorkingNodes/node00_Office/GW.h
@@ -2,10 +2,12 @@
 =========1=========2=========3=========4=========5=========6=========7========*/
 //#define MY_DEBUG                               // Enable debug prints to serial monitor
 //#define MY_DEBUG_VERBOSE_RF24                  // Enable for verbose debug prints related to the RF24 driver. Requires DEBUG to be enabled.
-
 #define MY_RADIO_NRF24                           // Enable and select radio type attached
-#define MY_RF24_PA_LEVEL RF24_PA_MAX             // Set radio power
+
+
+#define MY_RF24_PA_LEVEL RF24_PA_HIGH            // Set radio power
 #define MY_RF24_DATARATE RF24_250KBPS            // Set radio transmission rate
+//#define MY_RF24_DATARATE RF24_1MBPS
 
 /* MQTT definitions
 =========1=========2=========3=========4=========5=========6=========7========*/
@@ -16,15 +18,15 @@
 
 /* w5100 setup
 =========1=========2=========3=========4=========5=========6=========7========*/
-//#define MY_W5100_SPI_EN 4                      // W5100 Ethernet module SPI enable (keep disabled)
+//#define MY_W5100_SPI_EN 4                     // W5100 Ethernet module SPI enable (keep disabled)
 #if !defined(MY_W5100_SPI_EN) && !defined(ARDUINO_ARCH_SAMD)
-  #define MY_SOFTSPI                             // Enable Soft SPI for NRF radio (note different radio wiring is required)
-  #define MY_SOFT_SPI_SCK_PIN 14                 // (A0)
-  #define MY_SOFT_SPI_MOSI_PIN 15                // (A1)
-  #define MY_SOFT_SPI_MISO_PIN 16                // (A2)
+  #define MY_SOFTSPI                            // Enable Soft SPI for NRF radio (note different radio wiring is required)
+  #define MY_SOFT_SPI_SCK_PIN 14                // (A0)
+  #define MY_SOFT_SPI_MOSI_PIN 15               // (A1)
+  #define MY_SOFT_SPI_MISO_PIN 16               // (A2)
 #endif  
-#define MY_RF24_CE_PIN 5                         // For W5100 we have to move CE pin for radio
-#define MY_RF24_CS_PIN 6                         // For W5100 we have to move CSN pin for radio
+#define MY_RF24_CE_PIN 5                        // For W5100 we have to move CE pin for radio
+#define MY_RF24_CS_PIN 6                        // For W5100 we have to move CSN pin for radio
 
 /* MQTT broker definitions
 =========1=========2=========3=========4=========5=========6=========7========*/
@@ -35,25 +37,27 @@
 
 #define MY_MQTT_USER "AD"
 #define MY_MQTT_PASSWORD "AD"
-#define MY_CONTROLLER_IP_ADDRESS 10,0,0,32
-#define MY_PORT 1883
+//#define MY_CONTROLLER_IP_ADDRESS 10,0,0,5     // dulovic mycloud
+#define MY_CONTROLLER_IP_ADDRESS 10,0,0,4       // raspberry pi
+#define MY_PORT 1883    // 5003 ?
 
 
 /* IP Address (or DHCP)
 =========1=========2=========3=========4=========5=========6=========7========*/
-#define MY_IP_ADDRESS 10,0,0,33                   // Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
-#define MY_IP_GATEWAY_ADDRESS 10,0,0,1            // If using static ip you need to define Gateway and Subnet address as well
-#define MY_IP_SUBNET_ADDRESS 255,255,255,0        // When this doesnt work, check what pins are being used!!!
+#define MY_IP_ADDRESS 10,0,0,9                  // Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
+#define MY_IP_GATEWAY_ADDRESS 10,0,0,1          // If using static ip you need to define Gateway and Subnet address as well
+#define MY_IP_SUBNET_ADDRESS 255,255,255,0      // When this doesnt work, check what pins are being used!!!
 
 /* Optional & Additional
 =========1=========2=========3=========4=========5=========6=========7========*/
 //#define MY_RF24_CHANNEL 76                    // Change radio channel, has to be done on all nodes
-
 //#define MY_SIGNING_ATSHA204                   // Enables hardware signing using ATSHA204
 //#define MY_SIGNING_SOFT                       // Enables software signing
-
 //#define MY_PARENT_NODE_ID                             
 //#define MY_PARENT_NODE_IS_STATIC
-
 //#define MY_DISABLED_SERIAL                    // Enable if you want to use TX(1), RX(0) as normal I/O pin
 //#define MY_RF24_ENABLE_ENCRYPTION
+//#define MY_SIGNAL_REPORT_ENABLED              // Enables signal report functionality.
+//#define MY_TRANSPORT_SANITY_CHECK             // Check transport in regular intervals to detect HW issues and re-initialize in case of failure
+//#define MY_TRANSPORT_SANITY_CHECK_INTERVAL_MS (15*60*1000ul)
+
--- WorkingNodes/node00_Office/node00_Office.ino
+++ WorkingNodes/node00_Office/node00_Office.ino
@@ -1,9 +1,10 @@
 /**
- * Gateway in Office
+    Sketch Information
 =========1=========2=========3=========4=========5=========6=========7========*/
-#define MY_NODE_ID 00                   // (1-254) to manually set Node ID
 #define SN "Office (Gateway)"           // short Sketch (Node) name
-#define SV "1.170930"                   // short Sketch (Node) version
+#define SV "18-01-30"                   // short Sketch (Node) version
+#define MY_NODE_ID 00                   // (1-254) to manually set Node ID
+const bool Printout = false;             // set to false after testing
 
 /**
  * Library Includes
@@ -12,79 +13,63 @@
 #include <Ethernet.h>                   // Ethernet library for W5100 shield
 #include "GW.h"                         // NRF24L01 definitions for Gateway
 #include <MySensors.h>                  // MySensors library
+#define MQTT_MAX_PACKET_SIZE 512
 
 /**
  * Sensors and Activators
 =========1=========2=========3=========4=========5=========6=========7========*/
 
     /**
-     * A1 - Relay for projector power cord
+     * S01 - Reed switch, office door
      */
-    #define A1              1               // Sensor number
-    const int pinA1 =       2;              // Data pin
-    #include "A01_RelayInverted.h"          // Relay - for Projector Power
-
-    /**
-     * S1 - Reed switch - office door
-     */
-    #define S1              2               // Sensor number
-    const int pinS1 =       4;              // Data pin (with pull-up resistor)
-    int valueS1 = 0;                        // reading the sensor status
-    int stateS1 = 0;                        // storing the last status
-    #include "S01_ReedSwitch.h"             // Reed switch - Office Door
+    #define S01             2           // Sensor number
+    #define pinS01          4           // data pin with pull-up resistor
+    bool armedS01;                      // is armed?
+    bool trippedS01;                    // is tripped?
+    unsigned long tS01;                 // last time loop fired
+    #include "S01_ReedSwitch.h"         // from library
 
     /**
      * S2 - Temperature & Humidity (DHT 11)
      *      This one has two sensors on the same pin
      */
-    #define S2T             3               // Sensor number - Temperature
-    #define S2H             4               // Sensor number - Humidity
-    const int pinS2 =       3;              // data pin
-    float stateS2H;                         // last saved humidity value
-    float stateS2T;                         // last saved temperature value
-    unsigned long tS2 = 0;                  // last saved time the loop fired
-    unsigned long iS2 = 5;                  // interval at which to fire loop (in minutes)
-    #include <DHT.h>                        // DHT11 library
-    #include "S02_DHT11.h"                  // DHT 11 for Humidity and Temperature
+    #define S02t            3           // Sensor number - Temperature
+    #define S02h            4           // Sensor number - Humidity
+    #define pinS02          3           // data pin
+    unsigned long tS02;                 // last time loop fired
+    #include "S02_DHT11.h"              // from library
 
 /**
  * Presentation
 =========1=========2=========3=========4=========5=========6=========7========*/
 void presentation(){ 
-    /* Present the node */
     sendSketchInfo(SN, SV);
-    /* Present the Actuators and Sensors */
-    present(A1, S_BINARY, "Projector Power");
-    present(S1, S_DOOR, "Office ReedSwitch");
-    present(S2T, S_TEMP,"Office Temperature");
-    present(S2H, S_HUM,"Office Humidity");
-    /* Debug Printout */
-    // Serial.println((String)"\n === Node "+MY_NODE_ID+" presented ("+SN+")\n");
+    present(S01, S_DOOR, "Office ReedSwitch");
+    present(S02t, S_TEMP,"Office Temperature");
+    present(S02h, S_HUM,"Office Humidity");
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" presented ("+SN+")");}
 }
 
 /**
  * Setup
 =========1=========2=========3=========4=========5=========6=========7========*/
 void setup(){
-    setupA1();
-    setupS1();
-    setupS2();
-    /* Debug Printout */
-    // Serial.println((String)"\n === Node "+MY_NODE_ID+" setup ("+SN+")\n");
+//    setupS01();
+    setupS02();
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" setup ("+SN+")");}
 }
 
 /**
- * Loop ... once a second only
+ * Loop
 =========1=========2=========3=========4=========5=========6=========7========*/
 void loop(){
-    loopS1();
-    loopS2();
-    wait(1000);
+//    loopS01(1);
+    loopS02(10*60);
 }
 
 /**
- * Receive ... messages to the actuator(s)
+ * Receive
 =========1=========2=========3=========4=========5=========6=========7========*/
 void receive(const MyMessage &message) {
-    receiveA1(message);
+   receiveS01(message);
 }
