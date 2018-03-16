3077aeb0f3d2af2ec94b91f9fb50698e19ff6bfe
adulovic <adulovic@platergroup.com>
2018-03-16 10:45:24 -0400

    Garage node

--- WorkingNodes/node06_Garage/node06_Garage.ino
+++ WorkingNodes/node06_Garage/node06_Garage.ino
@@ -1,49 +1,89 @@
+/**
+    Sketch Information
+=========1=========2=========3=========4=========5=========6=========7========*/
 #define SN "Garage"                     // short Sketch (Node) name
 #define SV "1.1"                        // short Sketch (Node) version
 #define MY_NODE_ID 6                    // (1-254) to manually set Node ID
-/*
-    Includes
+const bool Printout = false;            // set to false after testing
+
+/**
+    Library Includes
 =========1=========2=========3=========4=========5=========6=========7========*/
 #include "R.h"                          // NRF24L01 definitions
 #include <MySensors.h>                  // MySensors library
-#include "def.h"                        // Explanation and inital variables
-  #include "A1.h"                       // Relay - lights
-  #include "S1.h"                       // Reed - gate
-  #include "S4.h"                       // PIR
-  #include "S13.h"                      // UltraSonic
+
+/**
+    Sensors and Activators
+=========1=========2=========3=========4=========5=========6=========7========*/
+
+    /**
+     * A01 - Relay
+     */
+    #define A01                 1       // sensor number
+    #define pinA01              2       // data pin
+    bool stateA01;                      // is on ?
+    #include "A01_Relay_Inverted.h"     // from library
+
+    /**
+     * S01 - Reed switch
+     */
+    #define S01                 2       // sensor number
+    #define pinS01              7       // data pin with pull-up resistor
+    bool trippedS01;                    // is tripped ?
+    bool armedS01;                      // is armed ?
+    unsigned long tS01;                 // last time loop fired
+    #include "S01_ReedSwitch.h"         // from library
+
+    /**
+     * S04 - PIR switch
+     */
+    #define S04                 3       // sensor number
+    #define pinS04              3       // data pin
+    bool trippedS04;                    // is tripped ?
+    bool armedS04;                      // is armed ?
+    unsigned long tS04;                 // last time loop fired
+    #include "S04_PIR.h"                // from library
+
+    /**
+     * S13 - Sonar
+     */
+    #define S13                 4       // sensor number
+    #define pinS13T             4       // data pin TRIG
+    #define pinS13E             5       // data pin ECHO
+    unsigned long tS13;                 // last time loop fired
+    #include "S13_Ultrasonic.h"         // UltraSonic
 /*
     Presentation
 =========1=========2=========3=========4=========5=========6=========7========*/
 void presentation(){ 
-  sendSketchInfo(SN, SV);
-    present(A1, S_BINARY, "Garage Lights");
-    present(S1, S_DOOR, "Garage Gate");
-    present(S4, S_MOTION, "Garage PIR");
+    sendSketchInfo(SN, SV);
+    present(A01, S_BINARY, "Garage Lights");
+    present(S01, S_DOOR, "Garage Gate");
+    present(S04, S_MOTION, "Garage PIR");
     present(S13, S_DISTANCE, "Garage Parking");
-  Serial.println((String)" === Node "+MY_NODE_ID+" presented ("+SN+")");
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" presented ("+SN+")");}
 }
 /* 
     Setup
 =========1=========2=========3=========4=========5=========6=========7========*/
 void setup(){
-    setupA1();
-    setupS1();
-    setupS4();
+    setupA01();
+    setupS01();
+    setupS04();
     setupS13();
-  Serial.println((String)" === Node "+MY_NODE_ID+" setup ("+SN+")");
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" setup ("+SN+")");}
 }
 /*
     Loop
 =========1=========2=========3=========4=========5=========6=========7========*/
 void loop(){
-    loopS1();
-    loopS4();
-    loopS13();
-  wait(1000);
+    loopS01(1);
+    loopS04(1);
+    loopS13(1);
 }
 /*
     Receive
 =========1=========2=========3=========4=========5=========6=========7========*/
 void receive(const MyMessage &message) {
-    receiveA1(message);
+    receiveA01(message);
 }
