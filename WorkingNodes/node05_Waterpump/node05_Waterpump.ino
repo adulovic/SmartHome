18abb6711cab9daa089c5ed0bbc7da6f9268044a
adulovic <adulovic@platergroup.com>
2018-03-16 10:44:50 -0400

    WaterPump node

--- WorkingNodes/node05_Waterpump/node05_Waterpump.ino
+++ WorkingNodes/node05_Waterpump/node05_Waterpump.ino
@@ -1,46 +1,70 @@
+/**
+    Sketch Information
+=========1=========2=========3=========4=========5=========6=========7========*/
 #define SN "Water Pump"                 // short Sketch (Node) name
-#define SV "1.1"                        // short Sketch (Node) version
-#define MY_NODE_ID 5                    // (1-254) to manually set Node ID
-/*
-    Includes
+#define SV "1.1 17-12-12"               // short Sketch (Node) version
+#define MY_NODE_ID 05                   // (1-254) to manually set Node ID
+const bool Printout = false;            // set to false after testing
+
+/**
+ * Library Includes
 =========1=========2=========3=========4=========5=========6=========7========*/
 #include "R.h"                          // NRF24L01 definitions
 #include <MySensors.h>                  // MySensors library
-#include "def.h"                        // Explanation and inital variables
-  #include "A1_2.h"                     // Double relay
-  #include "S4.h"                       // PIR
-  #include "S6.h"                       // VAC sensor
+
+/**
+ * Sensors and Activators
+=========1=========2=========3=========4=========5=========6=========7========*/
+
+    /**
+     * A01 - Relay for LED strip
+     */
+    #define A01                 1       // sensor number
+    #define pinA01              3       // data pin
+    bool stateA01;                      // is on ?
+    #include "A01_Relay_Inverted.h"     // from library
+
+    /**
+     * S04 - PIR switch
+     */
+    #define S04                 2       // sensor number
+    #define pinS04              4       // data pin
+    bool trippedS04;                    // is tripped ?
+    bool armedS04;                      // is armed ?
+    unsigned long tS04;                 // last time loop fired
+    #include "S04_PIR.h"                // from library
+
 /*
     Presentation
 =========1=========2=========3=========4=========5=========6=========7========*/
 void presentation(){ 
-  sendSketchInfo(SN, SV);
-    present(A12A, S_BINARY, "Water Pump");
-    present(A12B, S_BINARY, "Water Pump Lights");
-    present(S4, S_MOTION, "Water Pump PIR");
-    present(S6, S_POWER, "Water Pump VAC");
-  Serial.println((String)" === Node "+MY_NODE_ID+" presented ("+SN+")");
+    sendSketchInfo(SN,SV);
+    present(A01,S_BINARY,"Water Pump Lights");    // V_STATUS
+    present(S04,S_MOTION,"Water Pump PIR");       // V_TRIPPED, V_ARMED
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" presented ("+SN+")");}
 }
+
 /*
     Setup
 =========1=========2=========3=========4=========5=========6=========7========*/
 void setup(){
-    setupA1_2();
-    setupS4();
-    setupS6();
-  Serial.println((String)" === Node "+MY_NODE_ID+" setup ("+SN+")");
+    setupA01();
+    setupS04();
+    if (Printout==true) {Serial.println((String)" === Node "+MY_NODE_ID+" setup ("+SN+")");}
 }
+
 /*
     Loop
 =========1=========2=========3=========4=========5=========6=========7========*/
 void loop(){
-    loopS4();
-    loopS6();
-  wait(1000);
+    loopA01();                  // loop constantly
+    loopS04(1);                 // loop every 1 second
 }
+
 /*
     Receive
 =========1=========2=========3=========4=========5=========6=========7========*/
 void receive(const MyMessage &message) {
-    receiveA1_2(message);
+   receiveA01(message);
+   receiveS04(message);
 }
